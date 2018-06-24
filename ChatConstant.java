49278f672638cfd6c36c46d31c9b0cb8e76f3f94
Yingkai Wu <1061148047@qq.com>
2018-06-24 15:32:10 +0800

    Add files via upload

---
+++ ChatConstant.java
@@ -0,0 +1,16 @@
+package com.caihang.ylyim.common;
+
+public class ChatConstant {
+    public static final int TEXT_SEND = 0;
+    public static final int TEXT_RECV = 1;
+
+    public static final int WORD_SEND = 2;
+    public static final int WORD_RECV = 3;
+
+
+    public static final int CONTENT_TYPE_TEXT = 0;
+    public static final int CONTENT_TYPE_WORD = 1;
+
+    public static final int DIRECTION_SEND = 0;
+    public static final int DIRECTION_RECV = 1;
+}
---
+++ SPConstant.java
@@ -0,0 +1,6 @@
+package com.caihang.ylyim.common;
+
+public class SPConstant {
+    public static final String MY_USERID = "myUserId";
+    public static final String MY_ADD = "myAdd";
+}
